d7a37065747f13615d6a877ef7359e2fd9fb3cef
cfprabhu <cfprabhu@yahoo.com>
2021-07-01 14:59:51 +0400

    todolist list view updated

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, View
+from django.views.generic import TemplateView, View, ListView
+from .models import TodoList
+
 
 # Create your views here.
 
-class Home(TemplateView):
+class Home(ListView):
     template_name = 'home.html'
+    model = TodoList
+    context_object_name = 'todo_list'  # if you not define this you will get the todo_object object_list
