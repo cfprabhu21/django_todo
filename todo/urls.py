b11c8b0909adc82393c35d8ba70e879c08606280
cfprabhu <cfprabhu@yahoo.com>
2021-07-07 21:21:11 +0400

    todolist form view added

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home, AddTodo
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('create_todo/', AddTodo.as_view(), name='addtodo'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.views.generic import TemplateView, View, ListView
+from django.views import View
 from .models import TodoList
+from .forms import TodoForm
 
 
 # Create your views here.
@@ -9,3 +11,13 @@ class Home(ListView):
     template_name = 'home.html'
     model = TodoList
     context_object_name = 'todo_list'  # if you not define this you will get the todo_object object_list
+
+
+class AddTodo(View):
+    form_class = TodoForm
+
+    def post(self, request):
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('todo:home'))
